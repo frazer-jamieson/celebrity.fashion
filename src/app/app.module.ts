4841783ea784428326e04b0bbd8ad593b57f73c3
Frazer Jamieson <frazer@master.localdomain>
2017-04-30 16:59:40 +0100

    Contact Form and Post service added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ContactForm } from './contact/contact-form.component';
+import { PostService } from './services/form-poster.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactForm
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [ PostService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
